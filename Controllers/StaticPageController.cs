664b7e550a6ddd1aa1efdbb8f88a3c12de57eb16
Bogdan <bogdanprisacaru02@gmail.com>
2025-10-03 20:37:40 +0300

    Lab2

---
+++ Controllers/StaticPageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StaticPageController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
